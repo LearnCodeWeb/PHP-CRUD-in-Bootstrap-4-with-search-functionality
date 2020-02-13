42aca0252f8a1e3114f1c9141e5a9e41d9054692
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2020-02-13 16:10:07 +0900

    Update browse-users.php
    date search

--- browse-users.php
+++ browse-users.php
@@ -57,6 +57,17 @@
 	if(isset($_REQUEST['userphone']) and $_REQUEST['userphone']!=""){
 		$condition	.=	' AND userphone LIKE "%'.$_REQUEST['userphone'].'%" ';
 	}
+	if(isset($_REQUEST['df']) and $_REQUEST['df']!=""){
+
+		$condition	.=	' AND DATE(dt)>="'.$_REQUEST['df'].'" ';
+
+	}
+	if(isset($_REQUEST['dt']) and $_REQUEST['dt']!=""){
+
+		$condition	.=	' AND DATE(dt)<="'.$_REQUEST['dt'].'" ';
+
+	}
+	
 	$userData	=	$db->getAllRecords('users','*',$condition,'ORDER BY id DESC');
 	?>
    	<div class="container">
@@ -97,6 +108,21 @@
 									<input type="tel" name="userphone" id="userphone" class="form-control" value="<?php echo isset($_REQUEST['userphone'])?$_REQUEST['userphone']:''?>" placeholder="Enter user phone">
 								</div>
 							</div>
+							<div class="col-sm-4">
+
+								<div class="form-group">
+
+									<label>Date</label>
+									<div class="input-group">
+										<input type="text" class="fromDate form-control hasDatepicker" name="df" id="df" value="" placeholder="Enter from date">
+										<div class="input-group-prepend"><span class="input-group-text">-</span></div>
+										<input type="text" class="toDate form-control hasDatepicker" name="dt" id="dt" value="" placeholder="Enter to date">
+										<div class="input-group-append"><span class="input-group-text"><a href="javascript:;" onclick="$('#df,#dt').val('');"><i class="fa fa-fw fa-sync"></i></a></span></div>
+									</div>
+
+								</div>
+
+							</div>
 							<div class="col-sm-2">
 								<div class="form-group">
 									<label>&nbsp;</label>
@@ -121,25 +147,34 @@
 						<th>User Name</th>
 						<th>User Email</th>
 						<th>User Phone</th>
+						<th class="text-center">Record Date</th>
 						<th class="text-center">Action</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php 
-					$s	=	'';
-					foreach($userData as $val){
-						$s++;
+					if(count($userData)>0){
+						$s	=	'';
+						foreach($userData as $val){
+							$s++;
 					?>
 					<tr>
 						<td><?php echo $s;?></td>
 						<td><?php echo $val['username'];?></td>
 						<td><?php echo $val['useremail'];?></td>
 						<td><?php echo $val['userphone'];?></td>
+						<td align="center"><?php echo date('Y-m-d',strtotime($val['dt']));?></td>
 						<td align="center">
 							<a href="edit-users.php?editId=<?php echo $val['id'];?>" class="text-primary"><i class="fa fa-fw fa-edit"></i> Edit</a> | 
 							<a href="delete.php?delId=<?php echo $val['id'];?>" class="text-danger" onClick="return confirm('Are you sure to delete this user?');"><i class="fa fa-fw fa-trash"></i> Delete</a>
 						</td>
+
 					</tr>
+					<?php 
+						}
+					}else{
+					?>
+					<tr><td colspan="6" align="center">No Record(s) Found!</td></tr>
 					<?php } ?>
 				</tbody>
 			</table>
@@ -150,6 +185,48 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    
+    <script>
+		$(document).ready(function() {
+			jQuery(function($){
+				  var input = $('[type=tel]')
+				  input.mobilePhoneNumber({allowPhoneWithoutPrefix: '+1'});
+				  input.bind('country.mobilePhoneNumber', function(e, country) {
+					$('.country').text(country || '')
+				  })
+			 });
+			 
+			 //From, To date range start
+			var dateFormat	=	"yy-mm-dd";
+			fromDate	=	$(".fromDate").datepicker({
+				changeMonth: true,
+				dateFormat:'yy-mm-dd',
+				numberOfMonths:2
+			})
+			.on("change", function(){
+				toDate.datepicker("option", "minDate", getDate(this));
+			}),
+			toDate	=	$(".toDate").datepicker({
+				changeMonth: true,
+				dateFormat:'yy-mm-dd',
+				numberOfMonths:2
+			})
+			.on("change", function() {
+				fromDate.datepicker("option", "maxDate", getDate(this));
+			});
+			
+			
+			function getDate(element){
+				var date;
+				try{
+					date = $.datepicker.parseDate(dateFormat,element.value);
+				}catch(error){
+					date = null;
+				}
+				return date;
+			}
+			//From, To date range End here	
+			
+		});
+	</script>
 </body>
 </html>
